1c45190cc4e7ff92806ac6a3076f1c70b5d8bb01
xiaoliuzi <qingpengliu@qq.com>
2016-04-15 18:47:54 +0800

    add tiny.c

--- src/tiny.c
+++ src/tiny.c
@@ -103,3 +103,106 @@ void clienterror(int fd, char *cause, char *errnum,
 
 
 }
+
+void read_requesthdrs(rio_t *rp)
+{
+
+	char buf[MAXLINE];
+
+	rio_readlineb(rp, buf, MAXLINE);
+	while(strcmp(buf, "\r\n")) {
+		rio_readlineb(rp, buf, MAXLINE);
+		printf("%s", buf);
+	}
+	return;
+}
+
+int parse_uri(char *uri, char *filename, char *cgiargs)
+{
+
+	char *ptr;
+
+	if (!strstr(uri, "cgi-bin")) {
+
+		strcpy(cgiargs, "");
+		strcpy(filename, ".");
+		strcat(filename, uri);
+		if (uri[strlen(uri)-1] == '/') {
+			strcat(filename, "home.html");
+		}
+
+		return 1;
+	} else {
+		ptr = index(uri, '?');
+		if (ptr) {
+			strcpy(cgiargs, ptr+1);
+			*ptr = '\0';
+		}
+		else {
+			strcpy(cgiargs, "");
+		}
+
+		strcpy(filename, ".");
+		strcat(filename, uri);
+		return 0;
+	}
+}
+
+void serve_static(int fd, char *filename, int filesize)
+{
+	int srcfd;
+	char *srcp, filetype[MAXLINE], buf[MAXBUF];
+
+	/* Send response headers to client */
+	get_filetype(filename, filetype);
+	sprintf(buf, "HTTP/1.0 200 OK\r\n");
+	sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
+	sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
+	sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
+	rio_writen(fd, buf, strlen(buf));
+
+	/* Send response body to client */
+	srcfd = open(filename, O_RDONLY, 0);
+	srcp = mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+	close(srcfd);
+	rio_writen(fd, srcp, filesize);
+	munmap(srcp, filesize);
+}
+
+/*
+ * get_filetype - derive file type from file name
+ */
+
+void get_filetype(char *filename, char *filetype)
+{
+	if (strstr(filename, ".html"))
+		strcpy(filetype, "text/html");
+	else if (strstr(filename, ".gif"))
+		strcpy(filetype, "image/gif");
+	else if (strstr(filename, ".jpg"))
+		strcpy(filetype, "image/jpeg");
+	else
+		strcpy(filetype, "text/plain");
+}
+
+
+void serve_dynamic(int fd, char *filename, char *cgiargs)
+{
+	
+	char buf[MAXLINE], *emptylist[] = { NULL };
+
+	/* Return first part of HTTP response */
+	sprintf(buf, "HTTP/1.0 200 OK\r\n");
+	rio_writen(fd, buf, strlen(buf));
+	sprintf(buf, "Server: Tiny Web Server\r\n");
+	rio_writen(fd, buf, strlen(buf));
+	if (fork() == 0) { /* child */
+		/* Real server would set all CGI vars here */
+		setenv("QUERY_STRING", cgiargs, 1);
+		Dup2(fd, STDOUT_FILENO);		/* Redirect stdout to client */
+		execve(filename, emptylist, environ);	/* Run CGI program */
+	}
+	wait(NULL);		/* Parent waits for and reaps child */
+}
+
+
